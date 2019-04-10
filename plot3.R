f39204944e5df4a0128c5488b89c7fa59fe99454
mabette <45785047+mabette@users.noreply.github.com>
2019-04-10 08:41:59 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -7,12 +7,13 @@ data_selected$Date <- as.Date(data_selected$Date, format="%d/%m/%Y")
 timing <- paste(as.Date(data_selected$Date), data_selected$Time)
 # converting classes
 data_selected$Timing <- as.POSIXct(timing)
+# opening PNG file
+png("plot3.png", width=480, height=480)
 # plotting data
 with(data_selected, {plot(Sub_metering_1~Timing, type="l", ylab="Global Active Power (kilowatts)", xlab="")
   lines(Sub_metering_2~Timing,col='Red')
   lines(Sub_metering_3~Timing,col='Blue')})
 # setting-up legend
 legend("topright", col=c("black", "red", "blue"),lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-# saving the plot
-png("plot3.png", width=480, height=480)
+# closing file
 dev.off()
